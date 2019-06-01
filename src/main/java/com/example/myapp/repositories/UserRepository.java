b7289231ad06a028eb94b675eeb1c934b342d355
Paulo <paulodelta01@hotmail.com>
2019-06-01 18:37:55 -0300

    Add table model for user and repository

---
+++ src/main/java/com/example/myapp/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.myapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.myapp.models.User;
+
+public interface UserRepository 
+  extends CrudRepository<User, Integer> {
+	
+}
